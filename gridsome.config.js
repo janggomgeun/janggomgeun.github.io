b219dd78400eafd1358feb7842b485bfb1ba3640
janggomgeun <janggomgeun@gmail.com>
2020-06-19 13:45:47 +0900

    Install gridsome-plugin-tailwindcss

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,5 +7,22 @@
 module.exports = {
   siteName: 'Gridsome',
   siteUrl: 'https://janggomgeun.github.io',
-  plugins: []
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      /**
+      * These are the default options. You don't need to set any options to get
+      * going. Seriously, you don't need to declare tailwind.config.js.
+
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true
+      }
+      */
+    }
+  ]
 }
