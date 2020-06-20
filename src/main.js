c44d7b4044bf6722e2a00d350f7ae0fa72babfbf
janggomgeun <janggomgeun@gmail.com>
2020-06-20 18:09:09 +0900

    Clean all unnecessary codes

--- src/main.js
+++ src/main.js
@@ -7,3 +7,4 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
+ 
---
+++ src/templates/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <div>
+      <h1>{{ $page.post.title }}</h1>
+      <p>{{ $page.post.date }} | {{ $page.post.timeToRead }} min read</p>
+    </div>
+    <div>
+      <p v-html="$page.post.content" />
+    </div>
+  </Layout>
+</template>
+<page-query>
+query Post ($path: String!) {
+   post: post (path: $path) {
+    id
+    title
+    content
+    date (format: "D MMMM YYYY")
+    timeToRead
+  }
+}
+</page-query>
