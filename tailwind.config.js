d6cd8f74482cf1281ee3fc1f66b68b768b7b9a53
janggomgeun <janggomgeun@gmail.com>
2020-06-23 21:01:24 +0900

    Install multi-column css for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 module.exports = {
   purge: [],
-  theme: {
-    extend: {},
+  theme: { // defaults to these values
+    columnCount: [ 1, 2, 3 ],
+    columnGap: { // will fallback to 'gap' || 'gridGap' values
+      // sm: '1rem',
+      // md: '1.5rem',
+      // lg: '2rem',
+    },
+    columnWidth: {
+      // sm: '120px',
+      // md: '240px',
+      // lg: '360px',
+    },
+    columnRuleColor: false, // will fallback to `borderColor` values
+    columnRuleWidth: false, // will fallback to `borderWidth` values
+    columnRuleStyle: [
+      'none', 'hidden', 'dotted', 'dashed', 'solid',
+      'double', 'groove', 'ridge', 'inset', 'outset',
+    ],
+    columnFill: [ 'auto', 'balance', 'balance-all' ],
+    columnSpan: [ 'none', 'all' ],
   },
-  variants: {},
-  plugins: [],
+ 
+  variants: { // all the following default to ['responsive']
+    columnCount: ['responsive'],
+    columnGap: ['responsive'],
+    columnWidth: ['responsive'],
+    columnRuleColor: ['responsive'],
+    columnRuleWidth: ['responsive'],
+    columnRuleStyle: ['responsive'],
+    columnFill: ['responsive'],
+    columnSpan: ['responsive'],
+  },
+ 
+  plugins: [
+    require('tailwindcss-multi-column')(), // no options to configure
+  ],
 }
