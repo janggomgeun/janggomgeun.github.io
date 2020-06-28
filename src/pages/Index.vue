adcbdcd370a470a7532b0137a5b3926c63ca0d52
janggomgeun <janggomgeun@gmail.com>
2020-06-28 18:15:10 +0900

    Update UI

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,58 +1,58 @@
 <template>
   <Layout>
-    <div class="ml-6">
-      <h1>
-        <span>interface</span> JangGomGeun
+    <div class="m-6 text-right">
+      <h1 class="antialiased text-6xl my-1">
+        <span class="text-2xl">interface</span> JangGomGeun
       </h1>
       <div>
-        <h2>wisdom</h2>
-        <ul>
-          <li>
-            <h3>quotes</h3>
-            <ul>
-              <li v-for="edge in $page.quotes.edges" :key="edge.node.id">
-                <g-link :to="edge.node.path">
-                  <div>
-                    <p>{{ edge.node.quote }}</p>
+        <div>
+          <h2 class="underline">wisdom</h2>
+          <ul>
+            <li class="flex flex-row-reverse">
+              <h3 class="flex-initial">quotes</h3>
+              <ul class="flex-initial">
+                <li v-for="edge in $page.quotes.edges" :key="edge.node.id">
+                  <div class="p-2">
+                    <div class="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
+                      <span class="inline-flex bg-indigo-600 text-white rounded-full h-6 px-3 justify-center items-center">new</span>
+                      <g-link :to="edge.node.path">
+                        <span class="inline-flex px-2">{{ edge.node.quote }}</span>
+                      </g-link>
+                    </div>
                   </div>
-                </g-link>
-                <br>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <h3>principles</h3>
-            <ul>
-              <li v-for="edge in $page.principles.edges" :key="edge.node.id">
-                <g-link :to="edge.node.path">
-                  <div>
-                    <p>{{ edge.node.title }}</p>
+                </li>
+              </ul>
+            </li>
+            <li class="flex flex-row-reverse">
+              <h3 class="flex-initial">principles</h3>
+              <ul class="flex-initial">
+                <li v-for="edge in $page.principles.edges" :key="edge.node.id">
+                  <div class="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
+                    <span class="inline-flex bg-blue-500 text-white rounded-full h-6 px-3 justify-center items-center">new</span>
+                    <g-link :to="edge.node.path">
+                      <span class="inline-flex px-2">{{ edge.node.title }}</span>
+                    </g-link>
                   </div>
-                </g-link>
-                <br>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <h3>words</h3>
-          </li>
-        </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h3>words</h3>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="underline">expertise</h2>
+          <ul>
+          </ul>
+        </div>
+        <div>
+          <h2 class="underline">thoughts</h2>
+        </div>
+        <div>
+          <h2 class="underline">projects</h2>
+        </div>
       </div>
-      <div>
-        <h2>expertise</h2>
-        <ul>
-        </ul>
-      </div>
-      <div>
-        <h2>thoughts</h2>
-      </div>
-      <div>
-        <h2>projects</h2>
-      </div>
-      <ul>
-        
-        
-      </ul>
     </div>
   </Layout>
 </template>
@@ -64,11 +64,6 @@ export default {
 };
 </script>
 <style>
-/* @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-* {
-  font-family: 'Ubuntu Mono', monospace;
-  font-size: 40px;
-} */
 </style>
 <page-query>
 query {
