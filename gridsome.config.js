d3b210bce75f24793b3b1144395e6ba17cc62513
Jang Woong Geun <janggomgeun@gmail.com>
2021-09-05 19:07:07 +0900

    Add the site metadata

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: "Jang Gom Geun",
+  siteUrl: "https://janggomgeun.github.io",
   plugins: []
 }
