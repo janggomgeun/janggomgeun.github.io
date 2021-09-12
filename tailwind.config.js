c566d862b5cc29e0353054bc67195ade829611c2
Jang Woong Geun <janggomgeun@gmail.com>
2021-09-12 21:36:13 +0900

    Purge content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 require("tailwindcss/plugin");
 
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      "./src/**/*.vue",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.html",
+      "./src/**/*.pug",
+      "./src/**/*.md",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
