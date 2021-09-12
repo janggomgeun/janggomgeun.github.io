160f30be3355039cf37aa1852a4c2b0d161a42c6
Jang Woong Geun <janggomgeun@gmail.com>
2021-09-12 14:38:03 +0900

    Outline

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,10 +6,23 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/about/swot">SWOT</g-link>
+        <g-link class="nav__link" to="/about/focus">Focus</g-link>
+        <g-link class="nav__link" to="/about/preferences">Preferences</g-link>
+        <g-link class="nav__link" to="/about/principles">Principles</g-link>
+        <g-link class="nav__link" to="/about/retrospectives"
+          >Retrospectives</g-link
+        >
         <g-link class="nav__link" to="/projects">Projects</g-link>
+        <g-link class="nav__link" to="/expertise/cheatsheets">Expertise</g-link>
+        <g-link class="nav__link" to="/expertise/how_it_works"
+          >How it works</g-link
+        >
+        <g-link class="nav__link" to="/expertise/what_is_it">What is it</g-link>
       </nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -21,6 +34,4 @@ query {
 }
 </static-query>
 
-<style>
-
-</style>
+<style></style>
