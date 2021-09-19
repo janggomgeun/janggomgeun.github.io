f1889493c452b9bee4035807cfe2230ef196e2da
Jang Woong Geun <janggomgeun@gmail.com>
2021-09-19 12:53:09 +0900

    Organize menu items

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -15,15 +15,16 @@
       <nav class="text-4xl">
         <div>
           <div class="rounded-lg p-4 bg-gray-900 text-white">
-            <g-link to="/interests">interests, </g-link>
+            <!-- <g-link to="/interests">interests, </g-link>
             <g-link to="/preferences">preferences, </g-link>
+            <g-link to="/values">values, </g-link>
             <g-link to="/principles">principles, </g-link>
             <g-link to="/retrospectives">retrospectives, </g-link>
             <g-link to="/analysis">analysis, </g-link>
-            <g-link to="/thoughts">thoughts, </g-link>
-            <g-link to="/projects">projects, </g-link>
-            <g-link to="/cheatsheets">cheatsheets, </g-link>
-            <g-link to="/mechanisms">mechanisms</g-link>
+            <g-link to="/thoughts">thoughts, </g-link> -->
+            <g-link to="/projects">projects</g-link>
+            <!-- <g-link to="/cheatsheets">cheatsheets, </g-link> -->
+            <!-- <g-link to="/mechanisms">mechanisms</g-link> -->
           </div>
         </div>
       </nav>
