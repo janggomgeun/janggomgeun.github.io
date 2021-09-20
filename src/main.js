5f3808960723a71e54fb42788c2c6c6f2ecace44
Jang Woong Geun <janggomgeun@gmail.com>
2021-09-20 11:49:20 +0900

    Rename the layout Default to Main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 require("~/main.css");
 
-import DefaultLayout from '~/layouts/Default.vue'
+import MainLayout from '~/layouts/Main.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
@@ -12,6 +12,6 @@ library.add(faGithub, faLinkedin, faYoutube)
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', MainLayout)
   Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 }
